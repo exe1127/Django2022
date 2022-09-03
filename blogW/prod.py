1b15a63d237b278b26429b7b13e551deabb81b6c
exe1127 <exeatlas1127@gmail.com>
2022-09-03 15:28:06 -0300

    new hosts

--- blogW/prod.py
+++ blogW/prod.py
@@ -3,3 +3,4 @@ from .settings import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com','https://*.127.0.0.1']
